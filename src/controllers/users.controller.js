4a4902bc9d863f06e127236792eb807dd4f39cb0
Joaquin Morillo <morillojoaquin@hotmail.com>
2023-05-31 01:08:26 +0200

    first commit

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,23 @@
+export const logoutController = (req, res) => { 
+    try {
+        req.session.destroy( error => {
+            if (error) {
+                console.log(error);
+                res.json({ message: error })
+            } else {
+                res.redirect('/views/login')
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const githubLoginPassportController = (req, res) => {
+    try {
+        req.session.email = req.user.email;
+        res.redirect('/views/products');
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserCollection = "users";
+
+const UserSchema = mongoose.Schema ({
+    first_name : String,
+    last_name : String,
+    email:{
+        type: String,
+        unique: true
+    },
+    age: Number,
+    password: String,
+    admin: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    loggedBy: {
+        type: String,
+        default: "local"
+    
+    }
+
+});
+
+export const usersModel = mongoose.model(UserCollection, UserSchema); 
+
